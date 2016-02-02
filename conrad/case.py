9295546e9f7df149d193acf9572e3f68e7f14de9
bungun <ungun@stanford.edu>
2016-02-02 15:19:45 -0800

    starting python classes

---
+++ conrad/case.py
@@ -0,0 +1,10 @@
+"""
+case.py docstring
+"""
+
+class Case(object):
+	"""Case object docstring"""
+
+	def __init__(self):
+		"""Case.__init__() docstring"""
+		pass
---
+++ conrad/structure.py
@@ -0,0 +1,3 @@
+"""
+structure.py docstring
+"""
