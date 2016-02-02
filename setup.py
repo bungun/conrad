2159cfee3f4001c66e6d46cc5eff2d36b9edfa5a
bungun <ungun@stanford.edu>
2016-02-02 14:45:32 -0800

    housekeeping: setup.py, travis, copyright, etc.

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='conrad',
+    version='0.0.1a',
+    author='Baris Ungun, Anqi Fu, Stephen Boyd',
+    author_email='ungun@stanford.edu',
+    url='http://github.com/bungun/conrad/',
+    package_dir={'conrad': 'conrad'},
+    packages=[],
+    license='GPLv3',
+    zip_safe=False,
+    description='A convex optimization framework for radiation therapy treament planning',
+    long_description=LONG_DESC,
+    install_requires=["cvxpy",
+                      "numpy >= 1.8",
+                      "toolz"],
+    use_2to3=True,
+)
