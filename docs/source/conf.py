484027bb2b596f650c83b42101de393f0e343dae
bungun <ungun@stanford.edu>
2016-02-02 15:04:12 -0800

    change docs theme to sphinx_rtd_theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,6 +14,8 @@
 
 import sys
 import os
+import sphinx_rtd_theme
+
 # to import conrad:
 sys.path.insert(0, os.path.abspath(os.path.join('..', '..')))
 __version__ = "0.0.1a"
@@ -116,7 +118,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -127,7 +129,7 @@ html_theme_options = {
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
