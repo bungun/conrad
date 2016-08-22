ae3b5435f08f1407c86d09ba77308b904858cb71
bungun <ungun@stanford.edu>
2016-08-22 15:54:53 -0700

    (docs) add Google-style docstring to defs.py
    Additionally, remove println method, since __future__.print_function
    serves the same purpose.

--- conrad/defs.py
+++ conrad/defs.py
@@ -1,4 +1,16 @@
 """
+Frequently used constants and utilities.
+
+Attributes:
+	CONRAD_DEBUG (bool): Toggle for debugging-specific code branches.
+	CONRAD_DEBUG_PRINT (bool): Toggle for debugging-specifc print
+		statements.
+	SOLVER_OPTIONS (:obj:`list` of :obj:`str`): Enumeration of solver
+		names usable with `cvpxy`.
+	MAX_VERBOSITY (int):
+	CONRAD_MATRIX_TYPES (:obj:`tuple`): Enumeration of `numpy` and
+		`scipy` matrix types used int CONRAD.
+
 Copyright 2016 Baris Ungun, Anqi Fu
 
 This file is part of CONRAD.
@@ -22,26 +34,30 @@ from numpy import nan, squeeze, array, ndarray
 from scipy.sparse import csr_matrix, csc_matrix
 from conrad.compat import *
 
-def println(*args):
-	print(args)
-
 CONRAD_DEBUG = getenv('CONRAD_DEBUG', False)
-CONRAD_DEBUG_PRINT = println if CONRAD_DEBUG else lambda x : None
+CONRAD_DEBUG_PRINT = print if CONRAD_DEBUG else lambda x : None
 
 SOLVER_OPTIONS = ['ECOS', 'SCS']
-MAX_VERBOSITY = 1
 
 CONRAD_MATRIX_TYPES = (ndarray, csr_matrix, csc_matrix)
 
 def vec(vectorlike):
+	""" Convert input to one-dimensional `numpy.ndarray`. """
 	return squeeze(array(vectorlike))
 
 def is_vector(vectorlike):
+	""" True if instance is one-dimensional `numpy.ndarray`. """
 	if isinstance(vectorlike, ndarray):
 		return len(vectorlike) == vectorlike.shape[0]
 	return False
 
 def sparse_or_dense(matrixlike):
+	"""
+	True if input is a CONRAD-recognized matrix type.
+
+	Accepted types include: two-dimensional `ndarray`, `csr_matrx` and
+	`csc_matrix`.
+	"""
 	if matrixlike is None:
 		return False
 
@@ -52,6 +68,7 @@ def sparse_or_dense(matrixlike):
 	return sparse or dense
 
 def positive_real_valued(val):
+	""" True if input is a positive scalar. """
 	if val is not None and val is not nan and isinstance(val, (int, float)):
 		if val > 0:
 			return True
@@ -59,6 +76,18 @@ def positive_real_valued(val):
 		return False
 
 def module_installed(name, version_string=None):
+	"""
+	Test whether queried module is installed.
+
+	Arguments:
+		name (:obj:`str`): Name of module to query.
+		version_string (:obj:`str`, optional): Specific module version
+			to query.
+
+	Returns:
+		bool: True if queried module has a matching string in dictionary
+			values returned by `pip.get_installed_distributions`.
+	"""
 	modules = get_installed_distributions()
 	index = -1
 
