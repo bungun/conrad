7743216226d79adfe05fa420bb75740bf704456b
Anqi Fu <anqi.fu.1987@gmail.com>
2019-12-13 19:49:04 -0800

    WIP Finish ADMM implementation of dynamic treatment and begin testing

--- conrad/fractionation/admm_funcs.py
+++ conrad/fractionation/admm_funcs.py
@@ -1,8 +1,11 @@
 import cvxpy
 import numpy as np
+import numpy.linalg as LA
 from cvxpy import *
+from time import time
 from multiprocessing import Process, Pipe
-from mpc_funcs import dose_penalty, health_penalty, rx_to_constrs
+from data_utils import pad_matrix, health_prognosis
+from mpc_funcs import dose_penalty, health_penalty, dyn_objective, rx_to_constrs
 
 def build_dyn_prob_dose(A_list, patient_rx, T_recov = 0):
 	T_treat = len(A_list)
@@ -14,7 +17,7 @@ def build_dyn_prob_dose(A_list, patient_rx, T_recov = 0):
 	d = vstack([A_list[t]*b[t] for t in range(T_treat)])   # Doses.
 	
 	# Dose penalty function.
-	obj = sum([dose_penalty(d_var[t], p_var[t], patient_rx["dose_weights"]) for t in range(T)])
+	obj = sum([dose_penalty(d[t], p[t], patient_rx["dose_weights"]) for t in range(T_treat)])
 	
 	# Prescribed dose in each period.
 	# TODO: Add more bounds on the prescription?
@@ -40,11 +43,10 @@ def build_dyn_prob_dose_period(A, patient_rx, T_recov = 0):
 	
 	# Prescribed dose in current period.
 	# TODO: Add more bounds on the prescription?
-	constrs = [p_t == patient_rx["dose"][t]]
+	constrs = [p_t == patient_rx["dose"]]
 	
 	# Additional dose constraints in period.
 	if "dose_constrs" in patient_rx:
-		dose_constrs_cur = {"lower": patient_rx["dose_constrs"]["lower"][t], "upper": patient_rx["dose_constrs"]["upper"][t]}
 		constrs += rx_to_constrs(d_t, dose_constrs_cur)
 	
 	prob_t = Problem(Minimize(obj), constrs)
@@ -58,7 +60,7 @@ def build_dyn_prob_health(F, G, h_init, patient_rx, T_treat, T_recov = 0):
 	d = Variable((T_treat,K), pos = True, name = "doses")
 	
 	# Health penalty function.
-	obj = sum([health_penalty(h_var[t+1], patient_rx["health_weights"]) for t in range(T)])
+	obj = sum([health_penalty(h[t+1], patient_rx["health_weights"]) for t in range(T_treat)])
 	
 	# Health dynamics for treatment stage.
 	# constrs = [h[0] == h_init, b >= 0]
@@ -86,57 +88,120 @@ def build_dyn_prob_health(F, G, h_init, patient_rx, T_treat, T_recov = 0):
 	prob = Problem(Minimize(obj), constrs)
 	return prob, h, d
 
-def dynamic_treatment_admm(A_list, F, G, h_init, patient_rx, T_recov = 0, health_map = lambda h,t: h, rho = 1.0, max_iter = 1000, *args, **kwargs):
-	T_treat = len(A_list)
+def dynamic_treatment_admm(A_list, F, G, h_init, patient_rx, T_recov = 0, health_map = lambda h,t: h, *args, **kwargs):
+	# Problem parameters.
+	max_iter = kwargs.pop("max_iter", 1000) # Maximum iterations.
+	rho = kwargs.pop("rho", 1/10)           # Step size.
+	eps_abs = kwargs.pop("eps_abs", 1e-6)   # Absolute stopping tolerance.
+	eps_rel = kwargs.pop("eps_rel", 1e-8)   # Relative stopping tolerance.
 	
+	# Validate parameters.
+	T_treat = len(A_list)
+	K = F.shape[1]
+	if max_iter <= 0:
+		raise ValueError("max_iter must be a positive integer.")
+	if rho <= 0:
+		raise ValueError("rho must be a positive scalar.")
+	if eps_abs < 0:
+		raise ValueError("eps_abs must be a non-negative scalar.")
+	if eps_rel < 0:
+		raise ValueError("eps_rel must be a non-negative scalar.")
+		
 	# Set up dose workers.
 	pipes = []
 	procs = []
 	for t in range(T_treat):
+		rx_cur = patient_rx.copy()
+		rx_cur["dose"] = patient_rx["dose"][t]
+		if "dose_constrs" in patient_rx:
+			rx_cur["dose_constrs"] = {"lower": patient_rx["dose_constrs"]["lower"][t], "upper": patient_rx["dose_constrs"]["upper"][t]}
+		if "health_constrs" in patient_rx:
+			rx_cur["health_constrs"] = {"lower": patient_rx["health_constrs"]["lower"][t], "upper": patient_rx["health_constrs"]["upper"][t]}
+			
 		local, remote = Pipe()
 		pipes += [local]
-		p_dose_t, b_t, p_t, d_t = build_dyn_prob_dose(A_list[t], patient_rx, T_recov)
-		procs += [Process(target=run_dose_worker, args=(remote, p_dose_t, d_t, *args, **kwargs))]
+		procs += [Process(target=run_dose_worker, args=(remote, A_list[t], rx_cur, T_recov, rho) + args, kwargs=kwargs)]
 		procs[-1].start()
 	
 	# Proximal health problem.
-	p_health, h, d_tld = build_dyn_prob_health(F, G, h_init, patient_rx, T_treat, T_recov)
+	prob_health, h, d_tld = build_dyn_prob_health(F, G, h_init, patient_rx, T_treat, T_recov)
 	d_new = Parameter(d_tld.shape, value = np.zeros(d_tld.shape))
 	u = Parameter(d_tld.shape, value = np.zeros(d_tld.shape))
 	penalty = (rho/2)*sum_squares(d_tld - d_new + u)
-	prox = p_health + Problem(Minimize(penalty))
+	prox = prob_health + Problem(Minimize(penalty))
 	
 	# ADMM loop.
-	for k in range(max_iter):
+	k = 0
+	finished = False
+	r_prim = np.zeros(max_iter)
+	r_dual = np.zeros(max_iter)
+	
+	start = time()
+	while not finished:
+		# TODO: Add verbose printout.
+		if k % 10 == 0:
+			print("Iteration:", k)
+            
 		# Collect and stack d_t^k for t = 1,...,T.
 		d_rows = [pipe.recv() for pipe in pipes]
+		d_prev = d_new.value
 		d_new.value = np.row_stack(d_rows)
 		
 		# Compute and send \tilde d_t^k.
 		prox.solve(*args, **kwargs)
 		for t in range(T_treat):
-			pipe[t].send(d_tld[t].value)
+			pipes[t].send(d_tld[t].value)
 		
 		# Receive and update u_t^k for t = 1,...,T.
 		u_rows = [pipe.recv() for pipe in pipes]
 		u.value = np.row_stack(u_rows)
 		
-		# TODO: Calculate residuals and check stopping rule.
+		# Calculate residuals.
+		r_prim_mat = d_new.value - d_tld.value
+		r_dual_mat = rho*(d_new.value - d_prev)
+		r_prim[k] = LA.norm(r_prim_mat)
+		r_dual[k] = LA.norm(r_dual_mat)
+		
+		# Check stopping criteria.
+		eps_prim = eps_abs*np.sqrt(T_treat*K) + eps_rel*np.maximum(LA.norm(d_new.value), LA.norm(d_tld.value))
+		eps_dual = eps_abs*np.sqrt(T_treat*K) + eps_rel*LA.norm(u.value)
+		k = k + 1
+		finished = k >= max_iter
+		# finished = k >= max_iter or (r_prim[k] <= eps_prim and r_dual[k] <= eps_dual)
+		for pipe in pipes:
+			pipe.send(finished)
 	
-	# TODO: Compute final objective, status, solve_time.
-	return {"beams": b.value, "doses": d_tld.value, "health": h.value}
+	# Receive final values of b_t^k and p_t^k.
+	bp_update = [pipe.recv() for pipe in pipes]
+	b_rows, p_rows = map(list, zip(*bp_update))
+	b_val = np.row_stack(b_rows)
+	p_val = np.row_stack(p_rows)
+	
+	[proc.terminate() for proc in procs]
+	end = time()
+	
+	# Construct full results.
+	b_all = pad_matrix(b_val, T_recov)
+	d_all = pad_matrix(d_tld.value, T_recov)
+	h_all = health_prognosis(F, h_init, T_treat + T_recov, G, d_all, health_map)
+	obj = dyn_objective(d_tld.value, h.value, p_val, patient_rx).value
+	
+	return {"obj": obj, "status": prox.status, "beams": b_all, "doses": d_all, "health": h_all, "prescribed": p_val, 
+			"primal": np.array(r_prim[:k]), "dual": np.array(r_dual[:k]), "num_iters": k, "solve_time": end - start}
 
-def run_dose_worker(pipe, p, d, *args, **kwargs):
-	# Proximal dose problem.
+def run_dose_worker(pipe, A, patient_rx, T_recov, rho, *args, **kwargs):
+	# Construct proximal dose problem.
+	prob_dose, b, p, d = build_dyn_prob_dose_period(A, patient_rx, T_recov)
 	d_new = Parameter(d.shape, value = np.zeros(d.shape))
 	u = Parameter(d.shape, value = np.zeros(d.shape))
 	penalty = (rho/2)*sum_squares(d - d_new - u)
-	prox = p + Problem(Minimize(penalty))
+	prox = prob_dose + Problem(Minimize(penalty))
 	
 	# ADMM loop.
-	while True:
+	finished = False
+	while not finished:
 		# Compute and send d_t^k.
-		prox.solve(*args. **kwargs)
+		prox.solve(*args, **kwargs)
 		pipe.send(d.value)
 		
 		# Receive \tilde d_t^k.
@@ -145,3 +210,9 @@ def run_dose_worker(pipe, p, d, *args, **kwargs):
 		# Update and send u_t^^k.
 		u.value += d_new.value - d.value
 		pipe.send(u.value)
+		
+		# Check if stopped.
+		finished = pipe.recv()
+	
+	# Send final b_t^k and p_t^k.
+	pipe.send((b.value, p.value))
---
+++ conrad/fractionation/test_admm.py
@@ -0,0 +1,80 @@
+import numpy as np
+from admm_funcs import *
+from data_utils import beam_to_dose_block, health_prognosis
+from plot_utils import plot_health, plot_treatment
+
+# Problem data.
+# m = 10000
+# n = 1000
+# K = 10
+m = 1000
+n = 100
+K = 4
+T_treat = 20
+T_recov = 14
+T = T_treat + T_recov
+
+# Prescription.
+rx_dose = np.array([66] + (K-1)*[0])
+w_under = K*[1]
+w_over = [2] + (K-1)*[1]
+rx_dose_weights = [w_under, w_over]
+rx_health_weights = K*[1]
+patient_rx = {"dose": rx_dose, "dose_weights": rx_dose_weights, "health_weights": rx_health_weights}
+
+# Beam-to-dose matrix.
+A_full = np.abs(100 + 10*np.random.randn(m,n))
+A_blocks = np.split(A_full, K)
+# A_rows = [np.sum(block, axis = 0) for block in A_blocks]
+A_rows = [np.mean(block, axis = 0) for block in A_blocks]
+A = np.row_stack(A_rows)
+
+# Dynamics matrices.
+# F = np.diag([1.02, 0.95, 0.90, 0.85, 0.70, 0.65, 0.50, 0.25, 0.20, 0.15])
+F = np.diag([1.02, 0.95, 0.90, 0.75])
+G = -np.eye(K)
+A_list = T_treat*[A]
+h_init = np.array([0.25] + (K-1)*[0])
+
+# Actual health status transition function.
+mu = 0
+sigma = 0.005
+h_noise = mu + sigma*np.random.randn(T,K)
+health_map = lambda h,t: h + h_noise[t]
+
+# Health prognosis.
+# h_prog = health_prognosis(h_init, F, T, w_noise = h_noise)
+h_prog = health_prognosis(F, h_init, T, health_map = health_map)
+curves = {"Untreated": h_prog}
+
+# Dose constraints.
+# dose_lower = np.full((T_treat,K), -np.inf)
+# dose_upper = np.full((T_treat,K), np.inf)
+# dose_lower[:,0] = np.concatenate([np.full(5, 25), np.full(15, -np.inf)])
+# dose_upper[:,0] = 75
+# dose_upper[:,1:] = 25
+# patient_rx["dose_constrs"] = {"lower": dose_lower, "upper": dose_upper}
+
+# Prescription per period.
+patient_rx["dose"] = np.tile(rx_dose/T_treat, (T_treat,1))
+
+# Dynamic treatment.
+res_dynamic = dynamic_treatment_admm(A_list, F, G, h_init, patient_rx, T_recov, health_map = health_map, solver = "MOSEK")
+print("Dynamic Treatment")
+print("Status:", res_dynamic["status"])
+print("Objective:", res_dynamic["obj"])
+print("Iterations:", res_dynamic["num_iters"])
+
+# Plot dynamic health and treatment curves.
+plot_health(res_dynamic["health"], curves = curves, stepsize = 10, T_treat = T_treat)
+plot_treatment(res_dynamic["doses"], stepsize = 10, T_treat = T_treat)
+
+# Dynamic treatment with MPC.
+# res_mpc = mpc_treatment_admm(A_list, F, G, h_init, patient_rx, T_recov, health_map = health_map, solver = "MOSEK")
+# print("\nMPC Treatment")
+# print("Status:", res_mpc["status"])
+# print("Objective:", res_mpc["obj"])
+
+# Plot dynamic MPC health and treatment curves.
+# plot_health(res_mpc["health"], curves = curves, stepsize = 10, T_treat = T_treat)
+# plot_treatment(res_mpc["doses"], stepsize = 10, T_treat = T_treat)
