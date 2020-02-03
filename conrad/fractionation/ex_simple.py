75af7e590a45f0be3de2ff2421a66464cca76ea8
anqif <anqif@stanford.edu>
2020-02-03 14:57:15 -0800

    Add another small OAR to simple example

--- conrad/fractionation/ex_simple.py
+++ conrad/fractionation/ex_simple.py
@@ -5,7 +5,20 @@ from data_utils import line_integral_mat
 
 n = 10   # Number of beams.
 m_grid = 10000
-n_grid = 100
+n_grid = 500
+
+r0 = 0.6
+theta0 = 7*np.pi/6
+x0 = r0*np.cos(theta0)
+y0 = r0*np.sin(theta0)
+x_width = 0.075
+y_width = 0.15
+
+def ellipse(x, y, center = (0, 0), width = (1, 1), angle = 0):
+	x0, y0 = center
+	x_width, y_width = width
+	return (((x - x0)*np.cos(angle) + (y - y0)*np.sin(angle))/x_width)**2 + \
+		   (((x - x0)*np.sin(angle) - (y - y0)*np.cos(angle))/y_width)**2 - 1
 
 # Create polar grid.
 theta = np.linspace(0, 2*np.pi, m_grid)
@@ -13,9 +26,18 @@ r = np.linspace(0, 1, n_grid)
 theta_grid, r_grid = np.meshgrid(theta, r)
 
 # Define structure regions.
-regions = np.zeros((n_grid, m_grid))
-regions[r_grid <= 0.35] = 1   # OAR.
-regions[(r_grid >= 0.5) & (r_grid <= 0.65) & (theta_grid <= np.pi/2)] = 2   # PTV.
+regions = np.zeros((n_grid, m_grid))   # Body voxels (k = 0).
+
+# OAR (k = 1).
+regions[r_grid <= 0.35] = 1
+
+# OAR (k = 2).
+x_grid = r_grid*np.cos(theta_grid)
+y_grid = r_grid*np.sin(theta_grid)
+regions[ellipse(x_grid, y_grid, (x0, y0), (x_width, y_width), np.pi/6) <= 0] = 2
+
+# PTV (k = 3).
+regions[(r_grid >= 0.5) & (r_grid <= 0.65) & (theta_grid <= np.pi/2)] = 3
 K = np.unique(regions).size
 
 # Plot regions.
