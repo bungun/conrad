e31b4a9268cd0da23c348ba97607faf5caf17308
Anqi Fu <anqi.fu.1987@gmail.com>
2020-02-20 00:19:38 -0800

    Modify simple example to use Cartesian coordinates with bundles of beams

--- conrad/fractionation/test_structures.py
+++ conrad/fractionation/test_structures.py
@@ -16,25 +16,25 @@ n_grid = 1000
 
 # n = 1000
 n_angle = 10
-n_bundle = 5
+n_bundle = 10
 offset = 0.01
 
 xlim = (-1,1)
 ylim = (-1,1)
 
-x_grid, y_grid, structures = simple_structures(m_grid, n_grid, xlim, ylim)
+x_grid, y_grid, regions = simple_structures(m_grid, n_grid, xlim, ylim)
 
 if SHOW_STRUCTS:
 	struct_kw = simple_colormap()
-	plot_structures(x_grid, y_grid, structures, **struct_kw)
+	plot_structures(x_grid, y_grid, regions, **struct_kw)
 
 if CALC_AMAT:
-	A, angles, d_vec = line_integral_mat(structures, angles = n_angle, n_bundle = n_bundle, offset = offset)
+	A, angles, d_vec = line_integral_mat(regions, angles = n_angle, n_bundle = n_bundle, offset = offset)
 	if PLOT_LINES:
 		beams = np.full((1, n_angle*n_bundle), 1)
 		struct_kw = simple_colormap()
-		struct_tuple = (x_grid, y_grid, structures)
+		struct_tuple = (x_grid, y_grid, regions)
 		plot_beams(beams, angles, d_vec, xlim = xlim, ylim = ylim)
 		# plot_beams(beams, angles, d_vec, structures = struct_tuple, struct_kw = struct_kw, xlim = xlim, ylim = ylim)
 	if SAVE_AMAT:
-		np.save("data/A_cardiod_rot_{0}x{1}-grid_{2}-angles_{3}-bundles.npy".format(m_grid, n_grid, n_angle, n_bundle), A)
+		np.save("data/A_cardioid_rot_{0}x{1}-grid_{2}-angles_{3}-bundles.npy".format(m_grid, n_grid, n_angle, n_bundle), A)
