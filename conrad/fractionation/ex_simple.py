d3dc67a6af96a723b71b35573bcd258c597230b1
Anqi Fu <anqi.fu.1987@gmail.com>
2020-02-24 22:21:09 -0800

    Modify simple example to make beams and doses look sensible

--- conrad/fractionation/ex_simple.py
+++ conrad/fractionation/ex_simple.py
@@ -18,22 +18,23 @@ n = n_angle*n_bundle   # Total number of beams.
 # Display structures on a polar grid.
 x_grid, y_grid, regions = simple_structures(n_grid, n_grid)
 struct_kw = simple_colormap()
-# plot_structures(x_grid, y_grid, regions, **struct_kw)
-# plot_structures(x_grid, y_grid, regions, filename = "ex_cardioid_structures.png", **struct_kw)
+plot_structures(x_grid, y_grid, regions, **struct_kw)
+# plot_structures(x_grid, y_grid, regions, filename = "ex_cardioid2_structures.png", **struct_kw)
 
 # Problem data.
 K = np.unique(regions).size   # Number of structures.
+# r_pixels = [np.sum(regions == k) for k in range(K)]
 A, angles, offs_vec = line_integral_mat(regions, angles = n_angle, n_bundle = n_bundle, offset = offset)
 # A = np.load("data/A_cardiod_rot_1000x1000-grid_10-angles_10-bundles.npy")
 # angles = np.linspace(0, np.pi, n_angle+1)[:-1]
 # offs_vec = offset*np.arange(-n_bundle//2, n_bundle//2)
-A = A/(100*n_grid)
 A_list = T*[A]
 
 # F = np.diag([1.02, 0.95, 0.90, 0.75])
 F = np.diag([1.02, 0.90, 0.75, 0.95])
 # G = -np.eye(K)
-G = -np.diag([1.0, 0.95, 5, 0.65])
+# G = -np.diag([1.0, 0.95, 5, 0.65])
+G = -np.diag([0.01, 0.95, 0.25, 0.0065])
 r = np.zeros(K)
 h_init = np.array([1] + (K-1)*[0])
 
@@ -45,26 +46,28 @@ curves = {"Untreated": h_prog}
 rx_health_weights = [K*[1], K*[1]]
 rx_health_goal = np.zeros(K)
 rx_dose_weights = K*[1]
-# rx_dose_goal = np.zeros(K)
-rx_dose_goal = np.full((K,), 0.25)
+rx_dose_goal = np.zeros(K)
+rx_dose_goal[0] = 10
+# rx_dose_goal = np.full((K,), 10)
 patient_rx = {"dose_goal": rx_dose_goal, "dose_weights": rx_dose_weights, \
 			  "health_goal": rx_health_goal, "health_weights": rx_health_weights}
 
 # Beam constraints.
-beam_upper = np.full((T,n), 1)
+beam_upper = np.full((T,n), 0.0015)
 patient_rx["beam_constrs"] = {"upper": beam_upper}
 
 # Dose constraints.
 dose_lower = np.zeros((T,K))
-dose_upper = np.full((T,K), 0.5)   # Upper bound on doses.
+dose_upper = np.full((T,K), 25)   # Upper bound on doses.
 patient_rx["dose_constrs"] = {"lower": dose_lower, "upper": dose_upper}
 
 # Health constraints.
 health_lower = np.zeros((T,K))
 health_upper = np.zeros((T,K))
-# health_lower[:,1:] = -2.5    # Lower bound on OARs.
-health_lower[:,1:] = -1.0
-# health_lower[:,1:] = -0.65
+# health_lower[:,1:] = -1.0    # Lower bound on OARs.
+health_lower[:,1] = -0.5
+health_lower[:,2] = -0.75
+health_lower[:,3] = -1.0
 health_upper[:15,0] = 1.5    # Upper bound on PTV for t = 1,...,15.
 health_upper[15:,0] = 0.05   # Upper bound on PTV for t = 16,...,20.
 patient_rx["health_constrs"] = {"lower": health_lower, "upper": health_upper}
@@ -77,6 +80,7 @@ print("Status:", res_dynamic["status"])
 print("Objective:", res_dynamic["obj"])
 print("Solve Time:", res_dynamic["solve_time"])
 # print("Iterations:", res_dynamic["num_iters"])
+# res_dynamic["beams"][res_dynamic["beams"] < 1e-6] = 0
 
 # Plot dynamic beam, health, and treatment curves.
 # Set beam colors on logarithmic scale.
@@ -90,8 +94,8 @@ plot_beams(res_dynamic["beams"], angles = angles, offsets = offs_vec, stepsize =
 plot_health(res_dynamic["health"], curves = curves, stepsize = 10, bounds = (health_lower, health_upper))
 plot_treatment(res_dynamic["doses"], stepsize = 10, bounds = (dose_lower, dose_upper))
 
-# plot_residuals(res_dynamic["primal"], res_dynamic["dual"], semilogy = True, filename = "ex_simple_admm_dyn_beams.png")
+# plot_residuals(res_dynamic["primal"], res_dynamic["dual"], semilogy = True, filename = "ex_cardioid2_admm_residuals.png")
 # plot_beams(res_dynamic["beams"], angles = angles, offsets = offs_vec, stepsize = 1, cmap = transp_cmap(plt.cm.Reds), norm = lc_norm, \
-#			structures = (x_grid, y_grid, regions), struct_kw = struct_kw, alpha = 0.05, filename = "ex_cardioid_dyn_beams.png")
-# plot_health(res_dynamic["health"], curves = curves, stepsize = 10, bounds = (health_lower, health_upper), filename = "ex_cardioid_dyn_health.png")
-# plot_treatment(res_dynamic["doses"], stepsize = 10, bounds = (dose_lower, dose_upper), filename = "ex_cardioid_dyn_doses.png")
+#			structures = (x_grid, y_grid, regions), struct_kw = struct_kw, alpha = 0.5, filename = "ex_cardioid2_dyn_beams.png")
+# plot_health(res_dynamic["health"], curves = curves, stepsize = 10, bounds = (health_lower, health_upper), filename = "ex_cardioid2_dyn_health.png")
+# plot_treatment(res_dynamic["doses"], stepsize = 10, bounds = (dose_lower, dose_upper), filename = "ex_cardioid2_dyn_doses.png")
